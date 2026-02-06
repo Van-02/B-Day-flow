67a9a8e0aa68cccaaf48958cde1f676f91b513b3
Van-02 <lalikivan02@gmail.com>
2026-02-06 20:41:30 -0300

    modify templates.py

--- templates.py
+++ templates.py
@@ -1,3 +1,1 @@
-MESSAGES = {
-    "generic": "¡Hola {client}! Soy {seller} de LALIK BIKES San Rafael. Feliz cumpleaños!!! 🥳🎉🎊 Esperamos que este pequeño detalle sea de tu agrado, un saludo de todo el equipo.\nMuchas felicidades!!! 🍾🍰🎂 a pasarlo bien!! Que tengas un lindo dia. 😉"
-}
+MESSAGES = {"generic": "Generic message"}
